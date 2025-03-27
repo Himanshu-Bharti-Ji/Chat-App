a3eaeac29a31b12c669e8b7a126c363619b37598
Himasnhu <himanshubhartimax@gmail.com>
2025-03-27 22:17:29 +0530

    fix issue

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
 
   useEffect(() => {
     getUsers();
-    setShowOnlineOnly(true); 
+    // setShowOnlineOnly(true); 
   }, [getUsers, onlineUsers]);
 
   const filteredUsers = showOnlineOnly
